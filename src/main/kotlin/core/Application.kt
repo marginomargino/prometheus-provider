28b71eab29eb0455e2a0c3b023dcfef6920eaa8a
margino <vadim@widgetlabs.eu>
2024-10-26 12:38:21 +0400

    secure

--- src/main/kotlin/core/Application.kt
+++ src/main/kotlin/core/Application.kt
@@ -1,5 +1,7 @@
 package core
 
+import io.github.cdimascio.dotenv.dotenv
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -10,14 +12,37 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.receive
 
 fun main() {
+
+    val dotenv = dotenv {
+        ignoreIfMalformed = true
+        ignoreIfMissing = true
+    }
+
+    val accessToken = dotenv["ACCESS_TOKEN"] ?: error("ACCESS_TOKEN not set in .env")
+
+
     embeddedServer(Netty, port = 8080) {
+        install(createRouteScopedPlugin("BearerAuthPlugin") {
+            onCall { call ->
+                val authHeader = call.request.headers["Authorization"]
+
+                if (authHeader == null
+                    || !authHeader.startsWith("Bearer ")
+                    || authHeader.removePrefix("Bearer ") != accessToken
+                ) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@onCall
+                }
+            }
+        })
+
         install(ContentNegotiation) {
             json()
         }
         routing {
-            post("/ping") {
+            post("/sensors") {
                 log.info(call.receive<String>())
-                call.respondText("pong")
+                call.respond(HttpStatusCode.NoContent)
             }
         }
     }.start(wait = true)
